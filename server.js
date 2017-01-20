f1995a59da03d95b18724fbb0c5f59aa7e0a755e
GoldenPig72 <gideon.zondervan@live.nl>
2017-01-20 16:40:56 +0100

    Summary
    Initial rather sloppy code

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var http = require('http')
+var fs = require('fs')
+var app = express()
+
+var port = process.env.PORT || 8080;
+
+app.get('/', function (req, res) {
+//  res.send('Hello World!')
+
+fs.readFile('index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    });
+})
+
+
+})
+
+app.listen(port, function () {
+  console.log('Example app listening on port: ' + port)
+})
